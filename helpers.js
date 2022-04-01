9e7442844c1236badf37a2704eb810dde7c39e94
CYBERXKID <102310990+CYBERXKID@users.noreply.github.com>
2022-04-01 21:24:08 +0200

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *AMRU-SER*:  ```" + msg + "```"
+    return "✅ *CYBERXKID*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *AMRU-SER*:  ```" + msg + "```"
+    return "🛑 *CYBERXKID*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *AMRU-SER*:  ```" + msg + "```"
+    return "⏺️ *CYBERXKID*:  ```" + msg + "```"
 }
 
 
